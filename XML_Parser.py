5d1721d86e368f8500e531819e33530f2ceae00a
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2022-10-01 22:37:44 -0400

    Added an .info file parser to get the information from the .info file created by COMPASS

--- XML_Parser.py
+++ XML_Parser.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
 class XMLParser:
     def __init__(self, file: str):
@@ -28,4 +29,16 @@ class XMLParser:
                     self.parameters[tab][key] = value
 
         return self.parameters
-                
+
+class InfoParser:
+    def __init__(self, file: str):
+        self.file = file       
+
+    def get_run_info(self):
+        with open(self.file) as f:
+            informations = f.readlines()[0:4]
+        self.id = informations[0].split('=')[-1][:-1]
+        self.time_start = datetime.strptime(informations[1].split('=')[-1].split('.')[0], "%Y/%m/%d %H:%M:%S")
+        self.time_stop = datetime.strptime(informations[2].split('=')[-1].split('.')[0], "%Y/%m/%d %H:%M:%S")
+        self.time_real = self.time_stop - self.time_start
+        return self.id, self.time_start, self.time_stop, self.time_real
