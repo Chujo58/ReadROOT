71be0a054a4f5110777da172601c74682e1d90ae
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2024-03-17 13:01:56 -0400

    New changes for the error propagation file

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
 sys.path.append(path) #FOR C++ TO WORK!
 
-__version__ = '2.5.4'
+__version__ = '2.5.5'
 
 from . import read_root
 from . import read_root_gui
--- read_root_gui_v2.py
+++ read_root_gui_v2.py
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------------
 # Created by : Chloé Legué
-# Current version date : 2023/09/12
-# Version = 2.5.4
+# Current version date : 2024/03/17
+# Version = 2.5.5
 #----------------------------------------------------------------------------
 """
 This code was made for the coincidence experiment at McGill University. 
